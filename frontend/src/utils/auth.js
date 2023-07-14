c307ff4560b8ded112b8e9fb846bf8d29e4ce2a6
Max Teplyakov <maxxtepll@gmail.comgit config --listgit config --global user.email maxxtepll@gmail.com>
2023-07-15 02:16:35 +0400

    добавил aplication/json

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -11,6 +11,7 @@ export const register = (password, email) => {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
+      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ password, email }),
@@ -25,6 +26,7 @@ export const login = (password, email) => {
   return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
+      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ password, email }),
